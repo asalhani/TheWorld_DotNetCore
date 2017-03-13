c6079dfe8089dca027d717508a871c39fa8f1df0
asalhani <asalhani@outlook.com>
2017-03-13 09:58:29 +0300

    test commit

--- src/TheWorld/Startup.cs
+++ src/TheWorld/Startup.cs
@@ -16,7 +16,7 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-        }
+        }  
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
